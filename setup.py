b9000c32712e0041c2008593b986c4067ff3ae5b
Itakello <maxste000@gmail.com>
2024-03-13 20:01:24 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="itakello_logging",
-    version="0.1.0",
+    version="0.1.1",
     author="Itakello",
     author_email="maxste000@gmail.com",
     description="A custom logging library by Itakello",
